872a8d4e631423f2dfa3372fc9cc4099bf29fdd8
Jernej Kos <jernej@kos.mx>
2009-05-23 15:00:10 +0000

    Oooops forgot to commit collision groups:)

---
+++ collisiongroups.h
@@ -0,0 +1,21 @@
+/*
+ * This file is part of h0g.
+ *
+ * Copyright (C) 2009 by Jernej Kos <kostko@unimatrix-one.org>
+ * Copyright (C) 2009 by Anze Vavpetic <anze.vavpetic@gmail.com>
+ */
+#ifndef HOG_COLLISION_GROUPS_H
+#define HOG_COLLISION_GROUPS_H
+
+namespace HogCollisionGroup {
+
+/**
+ * Collision groups to identify objects.
+ */
+enum {
+  MovableObject = 32
+};
+
+}
+
+#endif
